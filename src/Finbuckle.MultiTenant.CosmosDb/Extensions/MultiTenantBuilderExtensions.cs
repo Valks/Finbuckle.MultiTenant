f44d0a78ce8239f1d9c81a28e70de1f2bf523d67
Wayne Adams <wadams@davey.com.au>
2020-10-01 10:14:26 +1000

    Add code to create the database if required.

--- src/Finbuckle.MultiTenant.CosmosDb/Extensions/MultiTenantBuilderExtensions.cs
+++ src/Finbuckle.MultiTenant.CosmosDb/Extensions/MultiTenantBuilderExtensions.cs
@@ -22,13 +22,32 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="databaseName"></param>
         /// <returns>The same <see cref="FinbuckleMultiTenantBuilder{TTenantInfo}"/> passed into the method.</returns>
         public static FinbuckleMultiTenantBuilder<TTenantInfo> WithCosmosDbStore<TTenantInfo>(this FinbuckleMultiTenantBuilder<TTenantInfo> builder,
-            CosmosClient client,
             string databaseName,
-            string collectionName)
+            string collectionName,
+            ThroughputProperties throughput = null)
             where TTenantInfo : class, ITenantInfo, new()
         {
+            if(throughput is null)
+            {
+                throughput = ThroughputProperties.CreateManualThroughput(400);
+            }
+
             builder.Services.AddSingleton<CosmosDbStoreContext>(services =>
             {
+                var client = services.GetRequiredService<CosmosClient>();
+                // Create the database and container if they don't exist.
+                var status = client.CreateDatabaseIfNotExistsAsync(databaseName, throughput)
+                                   .ConfigureAwait(true).GetAwaiter().GetResult().StatusCode;
+                Console.WriteLine($"Database Status: {status}");
+                if(status != System.Net.HttpStatusCode.OK && status != System.Net.HttpStatusCode.Created)
+                    throw new ApplicationException($"Failed to Open or Create Database. {databaseName}");
+                var database = client.GetDatabase(databaseName);
+                status = database.CreateContainerIfNotExistsAsync(new ContainerProperties(collectionName, $"/{nameof(ITenantInfo.Id).ToLower()}"), throughput)
+                                 .ConfigureAwait(false).GetAwaiter().GetResult().StatusCode;
+                Console.WriteLine($"Container Status: {status}");
+                if (status != System.Net.HttpStatusCode.OK && status != System.Net.HttpStatusCode.Created)
+                    throw new ApplicationException($"Failed to Open or Create Container. {collectionName}");
+
                 return new CosmosDbStoreContext(client.GetContainer(databaseName, collectionName));
             });
             return builder.WithStore<CosmosDbStore<TTenantInfo>>(ServiceLifetime.Scoped);
